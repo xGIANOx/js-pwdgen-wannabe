e869a263f5a01230b46a5d0f70c39d201ac73064
xGIANOx <studioenricogian@gmail.com>
2023-03-07 17:56:30 +0100

    [fix] code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-let user_name = prompt('Inserisci il tuo nome')
-console.log(user_name)
+const user_name = prompt('Inserisci il tuo nome');
+console.log(user_name);
 
-let user_surname = prompt('Inserisci il tuo cognome')
-console.log(user_surname)
+const user_surname = prompt('Inserisci il tuo cognome');
+console.log(user_surname);
 
-let favourite_colour = prompt('Inserisci il tuo colore preferito')
-console.log(favourite_colour)
+const favourite_colour = prompt('Inserisci il tuo colore preferito');
+console.log(favourite_colour);
 
-let passwordElement = document.getElementById('password')
-console.log(passwordElement)
+const passwordElement = document.getElementById('password');
+console.log(passwordElement);
 
-passwordElement.innerHTML= `${user_name}${user_surname}${favourite_colour}23`
+passwordElement.innerHTML= `${user_name}${user_surname}${favourite_colour}23`;
